febae8e07a5ab81a443792e9ee16c6c7ef5c4076
Michał Jung <stackustack@gmail.com>
2023-04-12 23:16:42 +0200

    3.technologies-graph-data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import { ForceGraph2D } from "react-force-graph";
-import { randomGraphData } from "./utils/randomGraphData";
+import { technologiesGraphData } from "./utils/technologiesGraphData";
 
 function App() {
-  return <ForceGraph2D graphData={randomGraphData({ numOfElements: 20 })} />;
+  // Calculate the maximum depth of the tree
+  const maxDepth = Math.max(
+    ...technologiesGraphData.nodes.map((node) => node.depth)
+  );
+
+  const getNodeSize = (node) => {
+    const depthSizeFactor = maxDepth - node.depth + 1;
+    return depthSizeFactor * 1;
+  };
+
+  return (
+    <ForceGraph2D
+      graphData={technologiesGraphData}
+      nodeLabel="label"
+      nodeAutoColorBy="group"
+      nodeVal={getNodeSize}
+      linkDirectionalArrowLength={6}
+      linkDirectionalArrowRelPos={1}
+    />
+  );
 }
 
 export default App;
